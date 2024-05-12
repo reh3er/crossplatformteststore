f68aa6d57263977497d259860c1765bc0182a1dd
reh3er <treasonsupsg@hotmail.com>
2024-05-13 05:49:08 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function quickSort(arr) {
-  if (arr.length <= 1) return arr;
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const left = [];
-  const right = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < pivot) left.push(arr[i]);
-    else if (arr[i] > pivot) right.push(arr[i]);
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  return quickSort(left).concat([pivot]).concat(quickSort(right));
-}
+  return gcd(b, a % b);
+};
